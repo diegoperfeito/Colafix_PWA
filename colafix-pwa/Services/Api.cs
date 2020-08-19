bf50f9a6324c7d372baa5f7fc6453f60d0966617
Diego Perfeito <dperfeito2011@gmail.com>
2020-08-18 21:54:23 -0300

    publicado

--- colafix-pwa/Services/Api.cs
+++ colafix-pwa/Services/Api.cs
@@ -21,8 +21,8 @@ namespace colafix_pwa.Services
         private static string BaseUrl()
         {
             //return "http://www.appcolafix.com.br:9292/Colafix/appservice.svc/";
-            return "http://localhost:11954/appservice.svc/";
-            // return "http://127.0.0.1:9292/Colafix/appservice.svc/";
+            //return "http://localhost:11954/appservice.svc/";
+            return "http://127.0.0.1:9292/Colafix/appservice.svc/";
             // return "http://200.98.172.139/Colafix/appservice.svc/";
         }
 
